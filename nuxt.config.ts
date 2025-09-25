257b644229fba6b15c6ae5b2c223c65d1ddcb7b9
GumYanka <gumyan777@gmail.com>
2025-09-25 21:10:01 +0300

    add github workflow to deploy app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  app: {
+    baseURL: "/calendar-vue/",
+  },
+  ssr: false,
+  nitro: {
+    output: {
+      dir: "dist",
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
