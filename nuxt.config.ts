87298c39bc260443da4c9de7389286f6c870534f
GumYanka <gumyan777@gmail.com>
2025-09-26 08:52:49 +0300

    Add GitHub Pages deploy workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
-    baseURL: "/calendar-vue/",
+    baseURL: process.env.NUXT_APP_BASE_URL || "/",
   },
   ssr: false,
   nitro: {
